f3bf6dedd15836cab004a2b92dc253aca5e50768
Artur Eger <artur.eger@eddie.pl>
2020-10-20 18:43:03 +0200

    Projekt koncowy 20201020

---
+++ src/main/java/com/example/demo/Koncowy20201020Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Koncowy20201020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Koncowy20201020Application.class, args);
+    }
+
+}
